e94b0358d54efefa22f9cfa64e82967320f3cca5
Matiaspotti <matias.pugliese@hotmail.com>
2020-03-18 16:25:23 -0300

    empezamos

---
+++ functions/index.js
@@ -0,0 +1,10 @@
+const functions = require("firebase-functions");
+const express = require("express");
+const app = express();
+
+app.use("/api/get", (req, res) => res.send("hello world"));
+
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+exports.app = functions.https.onRequest(app);
