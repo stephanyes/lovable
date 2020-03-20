6d71e4c58d7b400a92e8911f1720c3c3dfcf9c64
unknown <luztestai24@gmail.com>
2020-03-20 18:09:59 -0300

    mesas modificadas

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 //import axios from "axios";
 //import { connect } from "react-redux";
-import { Route, Redirect, Switch, Link } from "react-router-dom";
+//import { Route, Redirect, Switch, Link } from "react-router-dom";
 import Navbar from "./Navbar";
-import Tables from "./Tables";
+import Tables from "../containers/TablesContainer";
 import SidebarContainer from "../containers/SidebarContainer";
-import Login from "./Login";
+//import Login from "./Login";
 
 export default class Main extends React.Component {
   constructor(props) {
-    super(props);
+    super();
   }
   render() {
     return (
--- src/components/Tables.jsx
+++ src/components/Tables.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 // let random1 = Math.floor(Math.random() * 10) //va del 0 - 9
-export default ({ products }) => {
+export default ({ tables }) => {
   // console.log(products);
+
   return (
     <div
       style={{
@@ -18,148 +19,71 @@ export default ({ products }) => {
       >
         <h1 className="font-weight-bold">Tables</h1>
       </div>
+      {tables.length ? (
+        tables.map(table => (
+          <div class="row row-cols-1 row-cols-md-3">
+            <div
+              className="card mb-3"
+              style={{
+                width: "300px",
+                height: "125px",
+                margin: "30px",
+                padding: "0px",
+                borderStyle: "none",
+                boxShadow:
+                  "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+              }}
+            >
+              <div className="row no-gutters">
+                <div className="col-md-5">
+                  <img
+                    style={
+                      {
+                        // height: "140px",
+                        // width: "140px"
+                      }
+                    }
+                    src="https://insideone.s3-sa-east-1.amazonaws.com/services-table-busy.png"
+                    className="card-img"
+                    alt="..."
+                  />
+                </div>
+                <div className="col-md-7">
+                  <div
+                    className="card-body"
+                    style={{
+                      padding: "15px"
+                    }}
+                  >
+                    <h6
+                      className="font-weight-normal"
+                      //   style={{
+                      //     margin: "0px"
+                      //   }}
+                    >
+                      {table.state === "free" ? "On" : "Off"}
+                    </h6>
+                    <h3 className="font-weight-bold">Mesa: {table.number}</h3>
 
-      <div class="row row-cols-1 row-cols-md-3">
-        <div
-          className="card mb-3"
-          style={{
-            width: "300px",
-            height: "125px",
-            margin: "30px",
-            padding: "0px",
-            borderStyle: "none",
-            boxShadow:
-              "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
-          }}
-        >
-          <div className="row no-gutters">
-            <div className="col-md-5">
-              <img
-                style={
-                  {
-                    // height: "140px",
-                    // width: "140px"
-                  }
-                }
-                src="https://insideone.s3-sa-east-1.amazonaws.com/services-table-busy.png"
-                className="card-img"
-                alt="..."
-              />
-            </div>
-            <div className="col-md-7">
-              <div
-                className="card-body"
-                style={{
-                  padding: "15px"
-                }}
-              >
-                <h6
-                  className="font-weight-normal"
-                  //   style={{
-                  //     margin: "0px"
-                  //   }}
-                >
-                  Libre
-                </h6>
-                <h3 className="font-weight-bold">Mesa 1</h3>
-
-                <p
-                  className="font-weight-normal"
-                  style={{
-                    margin: "0px"
-                  }}
-                >
-                  <small class="text-muted">Mozo</small>
-                </p>
+                    <p
+                      className="font-weight-normal"
+                      style={{
+                        margin: "0px"
+                      }}
+                    >
+                      <small class="text-muted">
+                        Mozo: {table.waiter ? "Available" : "Occupied"}
+                      </small>
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="card mb-3" style={{ width: "450px", margin: "30px" }}>
-          <div className="row no-gutters">
-            <div className="col-md-4">
-              <img
-                src="https://insideone.s3-sa-east-1.amazonaws.com/services-table-free.png"
-                className="card-img"
-                alt="..."
-              />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <p className="card-text">Libre</p>
-                <h5 className="card-title">Mesa 1</h5>
-
-                <p className="card-text">
-                  <small class="text-muted">Mozo</small>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card mb-3" style={{ width: "450px", margin: "30px" }}>
-          <div className="row no-gutters">
-            <div className="col-md-4">
-              <img
-                src="https://insideone.s3-sa-east-1.amazonaws.com/services-table-pay.png"
-                className="card-img"
-                alt="..."
-              />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <p className="card-text">Libre</p>
-                <h5 className="card-title">Mesa 1</h5>
-
-                <p className="card-text">
-                  <small class="text-muted">Mozo</small>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card mb-3" style={{ width: "450px", margin: "30px" }}>
-          <div className="row no-gutters">
-            <div className="col-md-4">
-              <img
-                src="https://insideone.s3-sa-east-1.amazonaws.com/services-table-waiter.png"
-                className="card-img"
-                alt="..."
-              />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <p className="card-text">Libre</p>
-                <h5 className="card-title">Mesa 1</h5>
-
-                <p className="card-text">
-                  <small class="text-muted">Mozo</small>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="card mb-3" style={{ width: "450px", margin: "30px" }}>
-          <div className="row no-gutters">
-            <div className="col-md-4">
-              <img
-                src="https://insideone.s3-sa-east-1.amazonaws.com/services-table-order.png"
-                className="card-img"
-                alt="..."
-              />
-            </div>
-            <div className="col-md-8">
-              <div className="card-body">
-                <p className="card-text">Libre</p>
-                <h5 className="card-title">Mesa 1</h5>
-
-                <p className="card-text">
-                  <small class="text-muted">Mozo</small>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+        ))
+      ) : (
+        <h1>No hay tablas</h1>
+      )}
     </div>
   );
 };
