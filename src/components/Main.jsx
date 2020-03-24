1438008fdd81b25a291c6234880dafb49990a514
Stephano <stephanoiucciolino@hotmail.com>
2020-03-24 14:05:52 -0300

    login, restricciones al home sin user, y eliminar archivos que no usamos

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,28 +1,64 @@
 import React from "react";
-//import axios from "axios";
-//import { connect } from "react-redux";
-//import { Route, Redirect, Switch, Link } from "react-router-dom";
-import Navbar from "./Navbar";
+import { connect } from "react-redux";
+import { Route, Redirect, Switch, Link } from "react-router-dom";
+import Loader from 'react-loader-spinner'
 import Tables from "../containers/TablesContainer";
 import SidebarContainer from "../containers/SidebarContainer";
-
+import firebase from "../services/firebase"
 import LoginContainer from "../containers/LoginContainer";
 
-
-export default class Main extends React.Component {
-  constructor(props) {
-    super();
+const mapStateToProps = (state) => {
+  return {
+    userLogin: state.user.loginUser,
+    isAuth: state.user.isAuth
   }
+}
+class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      firebaseInitialized: false
+    };
+  }
+
+  componentDidMount() {
+    firebase.isInitialized().then(val => {
+      this.setState({
+        firebaseInitialized: val
+      })
+    })
+  }
+
+  // componentDidUpdate(prevProps) {
+  //   console.log(prevProps)
+  //   if (this.props.userLogin !== prevProps.userLogin) {
+  //     this.setState({
+  //       isAuth: "Logged"
+  //     })
+  //   }
+  // }
+  //Object.keys(this.props.userLogin).length === 0 ? <Redirect to="/" /> 
+
   render() {
+    console.log(this.props.isAuth)
     return (
-      <div>
-      <Navbar />
-        <Switch>
-          <Route exact path="/home" component={SidebarContainer}></Route>
-          <Route exact path="/" component={LoginContainer}></Route>
-        </Switch>
-        {/* <Tables /> */}
-      </div>
+      this.state.firebaseInitialized !== false ?
+        (
+          <div>
+            <Switch>
+              <Route exact path="/" component={LoginContainer}></Route>
+              <Route exact path="/home" component={SidebarContainer}></Route>
+            </Switch>
+            {/* <Tables /> */}
+          </div>
+        )
+        : (<div className="container" style={{ textAlign: "center", alignContent: "center" }}>
+          <Loader type="Hearts" color="red" height={80} width={80} />
+        </div >)
     );
   }
 }
+
+
+export default connect(mapStateToProps)(Main);
+
